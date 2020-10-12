865603a0e3e9c5eb15006ad27df696fecc9e63cd
Vishal Gaikwad <vishalgofficial@gmail.com>
2020-10-12 17:13:18 +0530

    Initial Commit

---
+++ src/test/java/com/promotion/engine/cartManagment/CartManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promotion.engine.cartManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
